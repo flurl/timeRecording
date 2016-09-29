c36674c8a65fe36fd607d580d3401da7d0a18516
flurl <it@postgarage.at>
2016-09-29 08:56:10 +0200

    created models for timeTracker app and added the app to the project

--- timeTracker/models.py
+++ timeTracker/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    number = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Shift(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    start = models.DateTimeField()
+    end = models.DateTimeField(null=True)
+
+
+class Break(models.Model):
+    shift = models.ForeignKey(Shift, on_delete=CASCADE)
+    start = models.DateTimeField()
+    end = models.DateTimeField(null=True)
