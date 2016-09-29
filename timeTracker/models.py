210a1568be468d9e2695e9840dfcfd646333b2de
flurl <it@postgarage.at>
2016-09-29 09:28:22 +0200

    added __str__() methods to the models

--- timeTracker/models.py
+++ timeTracker/models.py
@@ -9,14 +9,23 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return "%s - %s %s" % (self.number, self.last_name, self.first_name)
+
 
 class Shift(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     start = models.DateTimeField()
     end = models.DateTimeField(null=True)
 
+    def __str__(self):
+        return "%s: %s - %s" % (self.employee, self.start, self.end)
+
 
 class Break(models.Model):
     shift = models.ForeignKey(Shift, on_delete=CASCADE)
     start = models.DateTimeField()
     end = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return "%s - %s" % (self.start, self.end)
