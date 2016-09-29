4be6245afa53e82ab1a2be8537da4df44eb657b1
flurl <it@postgarage.at>
2016-09-29 14:59:53 +0200

    enabled admin interface for timeTracker app

--- timeTracker/admin.py
+++ timeTracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee, Shift, Break
+
+admin.site.register(Employee)
+admin.site.register(Shift)
+admin.site.register(Break)
