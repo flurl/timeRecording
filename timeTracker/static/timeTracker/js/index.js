4b37b840e47ea1680b5677fd445da703bdb8b0ee
flurl <it@postgarage.at>
2016-10-07 07:09:29 +0200

    Change Javascript variable names to camelCase

--- timeTracker/static/timeTracker/js/index.js
+++ timeTracker/static/timeTracker/js/index.js
@@ -1,11 +1,11 @@
-var time_tracker = (function() {
+var timeTracker = (function() {
 
-	var current_emp = null;
-	var current_shift = null;
+	var currentEmp = null;
+	var currentShift = null;
 	// the time from the server on which our calculations are based
 	// should be updated regularly for precision
-	var current_date = null;
-	var reload_timer = null;
+	var currentDate = null;
+	var reloadTimer = null;
 	
 	var userLocale = window.navigator.userLanguage || window.navigator.language;
 	var defaultDateFormat = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
@@ -13,42 +13,42 @@ var time_tracker = (function() {
 	return {
 	
 		setup: function() {
-			var emp_number_input = $('#emp_number');
-			emp_number_input.val(''); 
-			emp_number_input.focus();
-			emp_number_input.on('input', function(e) {
-				time_tracker.set_reload_timer();
-				var emp_number = $(this).val();
-				time_tracker.get_employee_info_by_number(emp_number);
+			var empNumberInput = $('#emp_number');
+			empNumberInput.val(''); 
+			empNumberInput.focus();
+			empNumberInput.on('input', function(e) {
+				timeTracker.setReloadTimer();
+				var empNumber = $(this).val();
+				timeTracker.getEmployeeInfoByNumber(empNumber);
 			});
-			emp_number_input.on('keypress', function(e) {
+			empNumberInput.on('keypress', function(e) {
 				if (e.which == 13) {
-					time_tracker.set_reload_timer();
-					time_tracker.on_enter_pressed();
+					timeTracker.setReloadTimer();
+					timeTracker.onEnterPressed();
 				}
 			});
-			this.sync_time();
-			this.set_reload_timer();
+			this.syncTime();
+			this.setReloadTimer();
 		},
 		
-		set_reload_timer: function() {
-			if (time_tracker.reload_timer) clearTimeout(time_tracker.reload_timer);
-			time_tracker.reload_timer = setTimeout(function() { location.reload(true); }, 60000);
+		setReloadTimer: function() {
+			if (timeTracker.reloadTimer) clearTimeout(timeTracker.reloadTimer);
+			timeTracker.reloadTimer = setTimeout(function() { location.reload(true); }, 60000);
 		},
 		
-		sync_time: function() {
+		syncTime: function() {
 			var self = this;
 			$.ajax({
 		        url : '/timeTracker/server_time/', // the endpoint
 		        type : "GET", // http method
 		        cache: false,
 		        
-		        complete: function() { setTimeout(time_tracker.sync_time, 5000)},
+		        complete: function() { setTimeout(timeTracker.syncTime, 5000)},
 		        
 		        success : function(response) {
-		        	self.current_date = new Date(parseInt(response)*1000);
+		        	self.currentDate = new Date(parseInt(response)*1000);
 		        	// TODO: is there a better way to determine the locale than the language?
-		        	$('#clock').text(self.current_date.toLocaleString(
+		        	$('#clock').text(self.currentDate.toLocaleString(
 		        										window.navigator.userLanguage || window.navigator.language, 
 		        										{ year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }
 		        										)
@@ -63,12 +63,12 @@ var time_tracker = (function() {
 		    });
 		},
 	
-		get_employee_info_by_number: function(emp_number) {
-			var err_div = $('#emp_error');
-			var info_div = $('#emp_info');
+		getEmployeeInfoByNumber: function(empNumber) {
+			var errDiv = $('#emp_error');
+			var infoDiv = $('#emp_info');
 			var self = this;
 			$.ajax({
-		        url : '/timeTracker/employee_info_by_number/'+emp_number+'/', // the endpoint
+		        url : '/timeTracker/employee_info_by_number/'+empNumber+'/', // the endpoint
 		        type : "GET", // http method
 		        cache: false,
 		        //data : { the_post : $('#post-text').val() }, // data sent with the post request
@@ -76,58 +76,58 @@ var time_tracker = (function() {
 		        // handle a successful response
 		        success : function(json) {
 		        	var emp = $.parseJSON(json)[0];
-		        	self.current_emp = emp.pk;
-		        	err_div.addClass('hidden');
-		        	info_div.removeClass('hidden');
+		        	self.currentEmp = emp.pk;
+		        	errDiv.addClass('hidden');
+		        	infoDiv.removeClass('hidden');
 		        	$('#emp_info_name').text(emp.fields.number + ' - ' + emp.fields.first_name + ' ' + emp.fields.last_name);
-		        	self.get_current_employee_shift(emp.pk);
+		        	self.getCurrentEmployeeShift(emp.pk);
 		        },
 		
 		        // handle a non-successful response
 		        error : function(xhr,errmsg,err) {
-		        	self.current_emp = null;
-		        	err_div.removeClass('hidden');
-		        	info_div.addClass('hidden'); 
-		            err_div.text('Unknown employee');
+		        	self.currentEmp = null;
+		        	errDiv.removeClass('hidden');
+		        	infoDiv.addClass('hidden'); 
+		            errDiv.text('Unknown employee');
 		            //console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
 		        }
 		    });
 		},
 		
-		get_current_employee_shift: function(emp_id) {
+		getCurrentEmployeeShift: function(empId) {
 			var self = this;
 			$.ajax({
-		        url : '/timeTracker/current_employee_shift/'+emp_id+'/', // the endpoint
+		        url : '/timeTracker/current_employee_shift/'+empId+'/', // the endpoint
 		        type : "GET", // http method
 		        cache: false,
 		        //data : { the_post : $('#post-text').val() }, // data sent with the post request
 		        
-		        complete: self.setup_actions.bind(self),
+		        complete: self.setupActions.bind(self),
 		
 		        // handle a successful response
 		        success : function(json) {
 		        	var shift = $.parseJSON(json)[0];
-		        	self.current_shift = shift.pk;
+		        	self.currentShift = shift.pk;
 		        	$('#emp_info_work_time').text(shift.fields.start);
 		        },
 		
 		        // handle a non-successful response
 		        error : function(xhr,errmsg,err) {
-		        	self.current_shift = null;
+		        	self.currentShift = null;
 		        	$('#emp_info_work_time').text('Not punched in');
 		        }
 		    });
 		},
 		
-		setup_actions: function() {
-			if (this.current_shift === null) {
-				this.setup_punch_in_actions();
+		setupActions: function() {
+			if (this.currentShift === null) {
+				this.setupPunchInActions();
 			} else {
-				this.setup_punch_out_actions();
+				this.setupPunchOutActions();
 			}
 		},
 		
-		setup_punch_in_actions: function() {
+		setupPunchInActions: function() {
 			var adiv = $('#actions');
 			adiv.empty();
 			adiv.append($('<h1>Punch in</h1>'));
@@ -135,30 +135,30 @@ var time_tracker = (function() {
 			var now = $('<a>', {
 				text: 'Now',
 				href: '#',
-				click: (function() {this.punch_in();}).bind(this)
+				click: (function() {this.punchIn();}).bind(this)
 			}).appendTo(adiv);
 			
-			let next_quarter_hour = new Date(this.current_date.getTime());
-			var minutes = (parseInt((next_quarter_hour.getMinutes() / 15)) + 1) * 15;
+			let nextQuarterHour = new Date(this.currentDate.getTime());
+			var minutes = (parseInt((nextQuarterHour.getMinutes() / 15)) + 1) * 15;
 			if (minutes >= 60) {
 				minutes = 0;
-				next_quarter_hour.setHours(next_quarter_hour.getHours()+1);
+				nextQuarterHour.setHours(nextQuarterHour.getHours()+1);
 			}
-			next_quarter_hour.setMinutes(minutes);
-			next_quarter_hour.setSeconds(0);
+			nextQuarterHour.setMinutes(minutes);
+			nextQuarterHour.setSeconds(0);
 			
 			for (var i=0; i<3; i++) {
-				let datetime=new Date(next_quarter_hour.getTime());
+				let datetime=new Date(nextQuarterHour.getTime());
 				datetime.setMinutes(datetime.getMinutes()+15*i);
 				var nowPlus = $('<a>', {
 					text: datetime.toLocaleString(userLocale, {hour: '2-digit', minute: '2-digit' }),
 					href: '#',
-					click: (function() {this.punch_in(datetime);}).bind(this)
+					click: (function() {this.punchIn(datetime);}).bind(this)
 				}).appendTo(adiv);
 			}
 		},
 		
-		setup_punch_out_actions: function() {
+		setupPunchOutActions: function() {
 			var adiv = $('#actions');
 			adiv.empty();
 			adiv.append($('<h1>Punch out</h1>'));
@@ -166,14 +166,14 @@ var time_tracker = (function() {
 			var now = $('<a>', {
 				text: 'Now',
 				href: '#',
-				click: (function() {this.punch_out();}).bind(this)
+				click: (function() {this.punchOut();}).bind(this)
 			}).appendTo(adiv);
 		},
 		
-		punch_in: function(when) {
+		punchIn: function(when) {
 			var when = (typeof when !== 'undefined') ?  when : null;
 			$.ajax({
-		        url : '/timeTracker/punch_in/' + this.current_emp + '/' + (when === null ? '' : when.getTime()/1000 + '/'), // the endpoint
+		        url : '/timeTracker/punch_in/' + this.currentEmp + '/' + (when === null ? '' : when.getTime()/1000 + '/'), // the endpoint
 		        type : "GET", // http method
 		        cache: false,
 		        //data : { the_post : $('#post-text').val() }, // data sent with the post request
@@ -195,10 +195,10 @@ var time_tracker = (function() {
 		    });
 		},
 		
-		punch_out: function(when) {
+		punchOut: function(when) {
 			var when = (typeof when !== 'undefined') ?  when : null;
 			$.ajax({
-		        url : '/timeTracker/punch_out/'+ this.current_shift + '/' + (when === null ? '' : when.getTime()/1000 + '/'), // the endpoint
+		        url : '/timeTracker/punch_out/'+ this.currentShift + '/' + (when === null ? '' : when.getTime()/1000 + '/'), // the endpoint
 		        type : "GET", // http method
 		        cache: false,
 		        //data : { the_post : $('#post-text').val() }, // data sent with the post request
@@ -221,12 +221,12 @@ var time_tracker = (function() {
 		    });
 		},
 		
-		on_enter_pressed: function() {
-			if (this.current_emp != null) {
-				if (this.current_shift !== null) {
-					this.punch_out();
+		onEnterPressed: function() {
+			if (this.currentEmp != null) {
+				if (this.currentShift !== null) {
+					this.punchOut();
 				} else {
-					this.punch_in();
+					this.punchIn();
 				}
 			}
 		},
@@ -234,5 +234,5 @@ var time_tracker = (function() {
 })();
 
 $(document).ready(function() {
-	time_tracker.setup();
+	timeTracker.setup();
 })
