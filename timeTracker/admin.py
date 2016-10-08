d9e3a337d5cd9332bc767c8c1a7273421c763a31
flurl <it@postgarage.at>
2016-10-08 15:16:41 +0200

    Improve change list view for shifts

--- timeTracker/admin.py
+++ timeTracker/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from .models import Employee, Shift, Break, FieldOfEmployment
 
+
+class ShiftAdmin(admin.ModelAdmin):
+    list_display = (
+        'employee', 'start', 'end', 'punch_in_forgotten', 'punch_out_forgotten')
+    list_filter = ('employee', 'start', 'punch_in_forgotten', 'punch_out_forgotten', 'field_of_employment')
+    search_fields = ['employee__first_name', 'employee__last_name']
+
+
+admin.site.register(Shift, ShiftAdmin)
+
 admin.site.register(Employee)
-admin.site.register(Shift)
 admin.site.register(Break)
 admin.site.register(FieldOfEmployment)
