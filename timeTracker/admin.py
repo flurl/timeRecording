cb6c9ea1542d4d502ef07354284ac6f5e4807363
flurl <it@postgarage.at>
2018-09-13 18:51:17 +0200

    Add the duration of a shift to the admin view

--- timeTracker/admin.py
+++ timeTracker/admin.py
@@ -4,10 +4,17 @@ from .models import Employee, Shift, Break, FieldOfEmployment
 
 
 class ShiftAdmin(admin.ModelAdmin):
+    readonly_fields = ('get_period',)
     list_display = (
-        'employee', 'start', 'end', 'punch_in_forgotten', 'punch_out_forgotten')
+        'employee', 'start', 'end', 'punch_in_forgotten', 'punch_out_forgotten', 'get_period')
     list_filter = ('employee', 'start', 'punch_in_forgotten', 'punch_out_forgotten', 'field_of_employment')
     search_fields = ['employee__first_name', 'employee__last_name']
+
+    def get_period(self, obj):
+        try:
+            return obj.end - obj.start
+        except TypeError as e:
+            return "open"
 
 
 admin.site.register(Shift, ShiftAdmin)
