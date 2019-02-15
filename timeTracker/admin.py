1b6846f93832c0253c1c64bbf338a005cb301f5e
flurl <it@postgarage.at>
2019-02-15 20:32:00 +0100

    Add a date range list filter to the shifts admin

--- timeTracker/admin.py
+++ timeTracker/admin.py
@@ -1,13 +1,57 @@
+from datetime import date, timedelta
+import time
+import calendar
+
 from django.contrib import admin
 
 from .models import Employee, Shift, Break, FieldOfEmployment
+
+class StartDateRangeListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = 'Date'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'daterange'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        x = 10
+        now = time.localtime()
+        months = [time.localtime(time.mktime((now.tm_year, now.tm_mon - n, 1, 0, 0, 0, 0, 0, 0)))[:2] for n in range(x)]
+
+        lookups = tuple([("%s-%s" % (month[0],month[1]), "%s-%02d" % (month[0],month[1])) for month in months])
+
+        return lookups
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        datestr = self.value()
+        if datestr:
+            year, month = [int(d) for d in datestr.split('-')]
+            start = date(year, month, 1)
+            end = date(year + (month+1)//12, (month+1)%12, 1)
+        return queryset.filter(start__gte=start,
+                               start__lt=end)
 
 
 class ShiftAdmin(admin.ModelAdmin):
     readonly_fields = ('get_period',)
     list_display = (
         'employee', 'start', 'end', 'punch_in_forgotten', 'punch_out_forgotten', 'get_period')
-    list_filter = ('employee', 'start', 'punch_in_forgotten', 'punch_out_forgotten', 'field_of_employment')
+    list_filter = (StartDateRangeListFilter, 'employee', 'start', 'punch_in_forgotten', 'punch_out_forgotten', 'field_of_employment')
     search_fields = ['employee__first_name', 'employee__last_name']
 
     def get_period(self, obj):
